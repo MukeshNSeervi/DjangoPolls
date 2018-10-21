613386675409cce142846a4642f3b0a2ec9ed5a4
NukeshNSeervi <ns1997mukesh@gmail.com>
2018-10-21 16:24:30 +0530

    complated tut-07

--- polls/views.py
+++ polls/views.py
@@ -7,13 +7,11 @@ from django.views     import generic
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = 'latest_question'
 
     def get_queryset(self):
-         return Question.objects.order_by('-pud_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #output = '\t\n'.join([q.question_text for q in latest_question_list])
-    #return HttpResponse(template.render(context,request))
+         return Question.objects.all()#order_by('-pud_date')[:5]
+
     #latest_question_list = Question.objects.order_by('-pud_date')[:5]
     #context = {'latest_question_list': latest_question_list}
     #return render(request,'polls/index.html',context)
@@ -24,20 +22,14 @@ class DetailView(generic.DetailView):
 
     #question = get_object_or_404(Question,pk=question_id)
     #return render(request,'polls/detail.html',{'question':question})
-    #try:
-    #    question = models.Question.objects.get(pk=question_id)
-    #except models.Question.DoesNotExist:
-    #    raise Http404("Question doesn't exist error")
-    #return render(request,'polls/detail.html',{'question':question })
-    #return HttpResponse("You're loking at question %s ." % question_id)
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-    #response = "You are looking at question %s ."
+
     #question = get_object_or_404(Question,pk=question_id)
     #return render(request ,"polls/results.html",{"question":question})
-    #return HttpResponse("You are looking at the result of question %s." % question_id)
 
 def vote(request,question_id):
 
